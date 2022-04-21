5bb7f9dc41978820445f0461f1e3b05dd7f3880f
Andy Chen <acchen@cpp.edu>
2022-04-20 23:15:29 -0700

    added result messages

--- chuckALuck.asm
+++ chuckALuck.asm
@@ -1,16 +1,17 @@
 # Andy Chen
 .data
 welcome_msg: .asciiz "Welcome to Chuck-a-luck.\n"
-input_prompt1: .asciiz "Enter a seed number: "
+input_prompt1: .asciiz "\nEnter a seed number: "
 input_prompt2: .asciiz "\nEnter your wager amount: "
 input_prompt3: .asciiz "What number do you want to bet on (1-6)? "
-input_prompt4: .asciiz "You currently have "
+input_prompt4: .asciiz "You currently have $"
 result_prompt1: .asciiz "\nI roll "
 result_prompt2: .asciiz "\nYou matched once.\n"
 result_prompt3: .asciiz "\nYou matched twice.\n"
 result_prompt4: .asciiz "\nYou matched thrice.\n"
 result_prompt5: .asciiz "\nYou did not match any.\n"
 error_prompt1: .asciiz "Your bet must be between 1 and 6!\n "
+error_prompt2: .asciiz "Your wager must be between zero and the balance amount!\n"
 seed: .word 19462
 .text
 
@@ -22,8 +23,11 @@ main:
 	#		$t2 = loop1 counter
 	#		$t3 = number of times random dice roll equals user dice roll bet
 	#		$t4 = hold random number that is returned from the rand routine
-	# 		$s0 = starting money of 500
-	# 		$s1 = 1 min bet value, $s2  = 6 max bet value
+	#		$t5 = hold value for which message to print
+	# 		$s0 = balance amount, initilized to 500
+	# 		$s1 = 1 min bet value
+	#	   $s2  = 6 max bet value
+	# 		$v0 = used for syscall and hold returning value from rand which will be saved in $t4
 	# Funtion Description:
 	#		This routine gets input for seed value and calls getUserInput for
 	#		the wager amout and their dice roll	bet.
@@ -33,22 +37,35 @@ main:
 	li $s0, 500
 	li $s1, 1
 	li $s2, 6
+	
+	# print all opening prompts
 	li $v0, 4
 	la $a0, welcome_msg
 	syscall
+	la $a0, input_prompt4
+	syscall
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	li $v0, 4
 	la $a0,input_prompt1
 	syscall
 	li $v0, 5
 	syscall
 	sw $v0, seed
+	
+	# jump to getUserInput routine
 	jal getUserInput
 	
 	# initialize loop counter to 3 and matching dice roll counter to 0
 	li $t2, 3
 	li $t3, 0
 	
+	# call rand three times and increment $t3 when $t1 (user bet) equals $t4 (random dice roll)
 	loop1: jal rand
-	move $t4, $v0 # save random number to $t4
+	move $t4, $v0		# save random number to $t4
+	
+	# print dice roll results
 	li $v0, 4
 	la $a0, result_prompt1
 	syscall
@@ -56,13 +73,44 @@ main:
 	move $a0, $t4
 	syscall
 	
-	bne $t1, $t4, skip
+	bne $t1, $t4, skip	# only increment if $1 == $t4
 	addiu $t3, $t3, 1
 	skip:
 	addi $t2, $t2, -1
-	bnez $t2, loop1
+	bnez $t2, loop1		# end loop
 	
-	li $v0, 10
+	# skip wager multiplication if $t3 (dice roll matches) == zero
+	beqz $t3, zeroMatch
+	mul $t0, $t0, $t3		# multiply wager amount by how many times dice roll matched user dice roll bet
+	mflo $t0
+	add $s0, $s0, $t0
+	
+	li $t5, 1
+	beq $t3, $t5, oneMatch
+	
+	li $t5, 2
+	beq $t3, $t5, twoMatch
+	
+	li $t5, 3
+	beq $t3, $t5, threeMatch
+	
+	li $v0, 4		# print string syscall
+	
+	zeroMatch: sub $s0, $s0, $t0
+	j printResult
+	
+	oneMatch: la $a0, result_prompt2
+	j printResult
+	
+	twoMatch: la $a0, result_prompt3
+	j printResult
+	
+	threeMatch: la $a0, result_prompt4
+	j printResult
+	
+	printResult: syscall
+	
+	end: li $v0, 10
 	syscall
 	
 getUserInput:
